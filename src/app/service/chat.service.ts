a9c02f2d237fc5292a04c6688e71355aecadf746
Sthiberson Preciado León <spreciado@greenperu.com>
2024-11-13 17:45:09 -0500

    feat: url actualizada

--- src/app/service/chat.service.ts
+++ src/app/service/chat.service.ts
@@ -8,7 +8,7 @@ import { Message } from '../model/message';
 })
 export class ChatService {
 
-  private apiUrl = 'http://localhost:8080/api/v1/messages';
+  private apiUrl = 'https://backend-tbkt.onrender.com/api/v1/messages';
 
   constructor(private http : HttpClient) { }
 
